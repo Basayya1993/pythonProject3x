73cca3837b17cc12a1481cd4faf11fa864908825
Basayya Hiremath <167414629+Basayya1993@users.noreply.github.com>
2024-04-28 15:41:52 +0530

    Initial commit

---
+++ Sample_Projects/Lab001.py
@@ -0,0 +1,7 @@
+name = "Praveen"
+
+print(len(name))
+print(name[2])
+print(len(name)-1)
+print(name[len(name)-1])
+print(name)
---
+++ Sample_Projects/Lab002.py
@@ -0,0 +1,6 @@
+num1= int (input("Num1:"))
+num2 = int (input("Num2:"))
+
+num3 = num1 / num2
+
+print(int(num3))
